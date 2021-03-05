9be59f3bc596f32124d02d32bb46b4a19c22ea1c
JRandle23 <jenny.randle23@gmail.com>
2021-03-05 12:38:18 -0800

    starting table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
 
+console.log(tableData);
+
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(ufoData) {
+    console.log(ufoData)
+});
+
+tableData.forEach(function(ufoData) {
+    console.log(ufoData);
+    var rows = tbody.append("tr")
+
+});
+
