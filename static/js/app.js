272675033207e35f64166c81b8b780827a1cf59e
JRandle23 <jenny.randle23@gmail.com>
2021-03-08 12:37:55 -0800

    fixing table data

--- static/js/app.js
+++ static/js/app.js
@@ -1,50 +1,22 @@
 // from data.js
 var tableData = data;
-
 console.log(tableData);
 
-// YOUR CODE HERE!
+// Reference the table body
 var tbody = d3.select("tbody");
 
-tableData.forEach(function(ufoData) {
-    console.log(ufoData)
-});
-
+// UFO sighting values for each column
 tableData.forEach(function(ufoData) {
     console.log(ufoData);
-    var rows = tbody.append("tr")
-
-});
-
-tableData.forEach(function(ufoData) {
-    console.log(ufoData);
-    var row = tbody.append("tr");
-
-    Object.entries(ufoData).forEach(function([key, value]) {
-        console.log(key, value)
-    });
-});
-
-tableData.forEach(function(ufoData) {
-    console.log(ufoData);
-    var row = tbody.append("tr");
+    // Append table row for each line of data 
+    var rows = tbody.append("tr");
 
     Object.entries(ufoData).forEach(function([key, value]) {
         console.log(key, value);
-
-        var cell = row.append("td");
-    });
-});
-
-
-tableData.forEach(function(ufoData) {
-    console.log(ufoData);
-    var row = tbody.append("tr");
-
-    Object.entries(ufoData).forEach(function([key, value]) {
-        console.log(key, value);
-
-        var cell = row.append("td");
+        // Append a cell to the row for each value
+        var cell = rows.append("td");
         cell.text(value);
     });
 });
+
+
