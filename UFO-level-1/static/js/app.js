667a8a65b137bd4166f0c660e5e76d95f124e34b
JRandle23 <jenny.randle23@gmail.com>
2021-03-09 22:16:56 -0800

    adding final filter code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,17 +6,89 @@ console.log(tableData);
 var tbody = d3.select("tbody");
 
 // UFO sighting values for each column
-tableData.forEach(function(ufoData) {
+tableData.forEach((ufoData) => {
     console.log(ufoData);
     // Append table row for each line of data 
     var rows = tbody.append("tr");
 
-    Object.entries(ufoData).forEach(function([key, value]) {
+    Object.entries(ufoData).forEach(([key, value]) => {
         console.log(key, value);
         // Append a cell to the row for each value
         var cell = rows.append("td");
         cell.text(value);
     });
 });
+
+
+// Assign data to variable
+//  var tableData = data;
+
+// Select button 
+var button = d3.select("#filter-btn");
+
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// function filteredTable(data) {
+//     tbody.html("");
+//     data.forEach((item) => {
+//         var newRow = tbody.append('tr');
+//         Object.values(item).forEach((val) => {
+//             newRow.append("td").text(val)
+//         });
+//     });
+// };
+
+function runEnter() {
+
+    // Prevent page refreshing 
+    d3.event.preventDefault();
+
+    // Select the input datetime element and get the raw HTML node
+    var inputElement = d3.select(".form-control");
+
+    //  Get the value property of the input element 
+    var inputValue = inputElement.property("value");
+
+    // console.log(inputValue);
+
+    // tbody.html("");
+
+        var filteredData = tableData.filter(ufoData => ufoData.datetime === inputValue);
+
+        tbody.html("");
+
+        if (filteredData.length === 0) {
+        tbody.text("No UFO sightings recorded.  Try again!");
+        }
+
+        else {
+            filteredData.forEach((ufoData) => {
+                var row = tbody.append("tr");
+                Object.entries(ufoData).forEach(([key,value]) => {
+                    var cell = row.append("td");
+                    cell.text(value);
+            });
+        });
+    };
+    // // console.log(filteredData);
+    // filteredData.forEach((ufoData) => {
+    //     var row = tbody.append("tr");
+    //     Object.entries(ufoData).forEach(([key,value]) => {
+    //         var cell = row.append("td");
+    //         cell.text(value);
+    //     });
+    // });
+    // console.log(filteredData);
+    // filteredTable(filteredData);
+
+};
+
+
+
+
 
 
